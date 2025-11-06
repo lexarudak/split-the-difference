7d9864e88f135d947588e0b6c4f59874c97a99db
a-rudak-EIS <arudak@ebsco.com>
2025-11-06 15:20:37 +0500

    feat: remove other

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useAuth } from "./hooks/useAuth";
 import LoginPage from "./components/LoginPage";
 import MainApp from "./components/MainApp";
 import LoadingSpinner from "./components/LoadingSpinner";
-import "./config-check";
 
 function App() {
 	const { user, loading } = useAuth();
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: "AIzaSyC9D9u-HLij2ardxa7WjU-iqFhkh7klw_I",
 	authDomain: "split-the-difference-2025.firebaseapp.com",
@@ -14,14 +12,10 @@ const firebaseConfig = {
 	measurementId: "G-9YLF5XWCL7",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
-
-// Initialize Google Auth Provider
 export const googleProvider = new GoogleAuthProvider();
 
 export default app;
